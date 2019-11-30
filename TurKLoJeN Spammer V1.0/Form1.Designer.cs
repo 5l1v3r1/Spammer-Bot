7f5dc05ae0055a96e4e4fcae5ed781b5772f8763
TurKLoJeN <32311900+TurKLoJeN@users.noreply.github.com>
2019-11-30 17:09:59 +0300

    Add files via upload

---
+++ TurKLoJeN Spammer V1.0/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace TurKLoJeN_Spammer_V1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ambiance_ThemeContainer1 = new Ambiance.Ambiance_ThemeContainer();
+            this.ambiance_Button_21 = new Ambiance.Ambiance_Button_2();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Button1 = new Ambiance.Ambiance_Button_2();
+            this.Button3 = new Ambiance.Ambiance_Button_2();
+            this.Button2 = new Ambiance.Ambiance_Button_1();
+            this.richTextBox1 = new Ambiance.Ambiance_RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ambiance_ThemeContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ambiance_ThemeContainer1
+            // 
+            this.ambiance_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(241)))), ((int)(((byte)(243)))));
+            this.ambiance_ThemeContainer1.Controls.Add(this.ambiance_Button_21);
+            this.ambiance_ThemeContainer1.Controls.Add(this.pictureBox1);
+            this.ambiance_ThemeContainer1.Controls.Add(this.Button1);
+            this.ambiance_ThemeContainer1.Controls.Add(this.Button3);
+            this.ambiance_ThemeContainer1.Controls.Add(this.Button2);
+            this.ambiance_ThemeContainer1.Controls.Add(this.richTextBox1);
+            this.ambiance_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ambiance_ThemeContainer1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.ambiance_ThemeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.ambiance_ThemeContainer1.Name = "ambiance_ThemeContainer1";
+            this.ambiance_ThemeContainer1.Padding = new System.Windows.Forms.Padding(20, 56, 20, 16);
+            this.ambiance_ThemeContainer1.RoundCorners = true;
+            this.ambiance_ThemeContainer1.Sizable = true;
+            this.ambiance_ThemeContainer1.Size = new System.Drawing.Size(800, 450);
+            this.ambiance_ThemeContainer1.SmartBounds = true;
+            this.ambiance_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.ambiance_ThemeContainer1.TabIndex = 1;
+            this.ambiance_ThemeContainer1.Text = "TurKLoJeN Spam Bot V1.0";
+            // 
+            // ambiance_Button_21
+            // 
+            this.ambiance_Button_21.BackColor = System.Drawing.Color.Transparent;
+            this.ambiance_Button_21.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.ambiance_Button_21.Image = null;
+            this.ambiance_Button_21.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ambiance_Button_21.Location = new System.Drawing.Point(768, 0);
+            this.ambiance_Button_21.Name = "ambiance_Button_21";
+            this.ambiance_Button_21.Size = new System.Drawing.Size(32, 33);
+            this.ambiance_Button_21.TabIndex = 5;
+            this.ambiance_Button_21.Text = "X";
+            this.ambiance_Button_21.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.ambiance_Button_21.Click += new System.EventHandler(this.ambiance_Button_21_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(754, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Button1
+            // 
+            this.Button1.BackColor = System.Drawing.Color.Transparent;
+            this.Button1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.Button1.Image = null;
+            this.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Button1.Location = new System.Drawing.Point(23, 372);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(233, 47);
+            this.Button1.TabIndex = 3;
+            this.Button1.Text = "Hakkında";
+            this.Button1.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // Button3
+            // 
+            this.Button3.BackColor = System.Drawing.Color.Transparent;
+            this.Button3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.Button3.Image = null;
+            this.Button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Button3.Location = new System.Drawing.Point(544, 372);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(233, 47);
+            this.Button3.TabIndex = 2;
+            this.Button3.Text = "Durdur";
+            this.Button3.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click_1);
+            // 
+            // Button2
+            // 
+            this.Button2.BackColor = System.Drawing.Color.Transparent;
+            this.Button2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.Button2.Image = null;
+            this.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Button2.Location = new System.Drawing.Point(283, 372);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(233, 47);
+            this.Button2.TabIndex = 1;
+            this.Button2.Text = "Başlat";
+            this.Button2.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.AutoWordSelection = false;
+            this.richTextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.richTextBox1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.richTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
+            this.richTextBox1.Location = new System.Drawing.Point(23, 122);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = false;
+            this.richTextBox1.Size = new System.Drawing.Size(754, 232);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.WordWrap = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ambiance_ThemeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(261, 65);
+            this.Name = "Form1";
+            this.Text = "TurKLoJeN Spam Bot V1.0";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.ambiance_ThemeContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Ambiance.Ambiance_ThemeContainer ambiance_ThemeContainer1;
+        private Ambiance.Ambiance_Button_2 ambiance_Button_21;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Ambiance.Ambiance_Button_2 Button1;
+        private Ambiance.Ambiance_Button_2 Button3;
+        private Ambiance.Ambiance_Button_1 Button2;
+        private Ambiance.Ambiance_RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TurKLoJeN Spammer V1.0/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TurKLoJeN_Spammer_V1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("TurKLoJeN Tarafından Kodlanmıştır Versiyon 1.0");
+        }
+
+        private void ambiance_Button_21_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send(richTextBox1.Text);
+            SendKeys.Send("{Enter}");
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("TurKLoJeN Tarafından Kodlanmıştır Versiyon 1.0");
+        }
+
+        private void Button2_Click_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Button3_Click_1(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            SendKeys.Send(richTextBox1.Text);
+            SendKeys.Send("{Enter}");
+        }
+
+        private void ambiance_Button_21_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+    }
+}
